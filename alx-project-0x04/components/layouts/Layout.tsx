dd78fb026f152f891a27a63639abec317191de8f
EL-HOUSS-BRAHIM <brahim.crafts.tech@gmail.com>
2025-06-15 23:18:13 +0100

    pushing Layout with the latest updates alx :|

--- alx-project-0x04/components/layouts/Layout.tsx
+++ alx-project-0x04/components/layouts/Layout.tsx
@@ -1,15 +1,14 @@
-import { ReactNode } from "react";
 import Footer from "./Footer";
 import Header from "./Header";
+import { LayoutProps } from "@/interface";
 
-import { LayoutProps } from "../../interface";
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
       <main>{children}</main>
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 };
